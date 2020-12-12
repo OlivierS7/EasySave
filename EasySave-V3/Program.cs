2472a58bd9a78c305cba08b2868c3cc21f52aab8
Vincentkayak <vincent.laclide@viacesi.fr>
2020-12-12 12:58:06 +0100

    Mono Instance Processus

--- EasySave-V3/Program.cs
+++ EasySave-V3/Program.cs
@@ -8,14 +8,21 @@ namespace EasySave_V2
 {
     static class Program
     {
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Controller controller = new Controller();
-            
+            bool createdNew;
+            new Mutex(true, "EasyaveRunning", out createdNew);
+            if (createdNew)
+            {
+                Controller controller = new Controller();
+            }
+            else
+                MessageBox.Show("An EasySave Process is already running.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
         }
     }
 }
